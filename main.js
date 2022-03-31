8bace5010be92ffb634219e41863bc7c3489ed02
Daewik <75206835+Daewik@users.noreply.github.com>
2022-03-31 15:31:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+function preload()
+{}
+
+function setup()
+{
+  canvas = createCanvas(300,300);
+  canvas.center();
+
+  video = createCapture(VIDEO);
+  video.size(300,300);
+  video.hide();
+
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
+}
+
+function draw()
+{
+  image(video,0,0,300,300)
+}
+
+function take(){
+    save('image.jpeg')
+}
+
+function modelLoaded(){
+  console.log('Model has been initialized');
+}
+
+function gotPoses(results){
+  if(results.length > 0)
+  {
+    console.log('nose x position =' + results[0].pose.nose.x + 'and nose y position = ' + results[0].pose.nose.y);
+  }
+}
